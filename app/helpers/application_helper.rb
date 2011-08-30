8b7cd8510a97542b0714f0379b2f3467d46654cc
Erwin <mannyhenri@blueyfit.com>
2011-08-31 00:03:23 +0200

    stable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def load_controller_js(script = controller.controller_name)
     content_for :extra_js do
-      javascript_include_tag "controllers/#{script}"
+     # javascript_include_tag "controllers/#{script}"
     end
   end
 
