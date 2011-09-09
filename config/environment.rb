c40548672e97c63138eb0d2041042dbba2eccf1f
Erwin <mannyhenri@blueyfit.com>
2011-09-09 16:19:27 +0200

    Safari 5 back

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Bluey::Application.initialize!
 Carmen.priority_countries = %w(US CA GB AR RU FR)
 
 Uaid.supported_agents = [
-  /safari [4]/,
+  /safari [5]/,
   /chrome [10]/,
   /firefox [5]/,
   /ie [9]/
