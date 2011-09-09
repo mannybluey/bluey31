cfb594d8f2a01495b43139ee5e64775c9ae9659b
Aldo Nievas <aldo@satio.com.ar>
2011-09-08 23:01:48 -0300

    Gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ Bluey::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
