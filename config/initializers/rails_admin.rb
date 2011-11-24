0c05a7c28c22737c2a71843f9b491766698b6218
Alex Duller <alexduller@gmail.com>
2011-11-24 14:52:21 +0000

    Set default_items_per_page to 20 in rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -53,7 +53,7 @@ RailsAdmin.config do |config|
 
   #  ==> Global list view settings
   # Number of default rows per-page:
-  # config.default_items_per_page = 20
+  config.default_items_per_page = 20
 
   #  ==> Included models
   # Add all excluded models here:
